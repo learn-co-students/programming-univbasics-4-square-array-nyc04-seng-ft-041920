933453ef8d9067aeafd11d3df299e0534021b808
Leonora Squires <leonora.squires@gmail.com>
2020-04-23 20:53:18 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,12 @@
 def square_array(array)
   count = 0
   new_array = []
+  
   while count < array.length do
-    new_var = array[count]*array[count]
+    new_var = array[count]**2
     new_array.push(new_var)
+    count+=1
   end
+  
   new_array
 end
